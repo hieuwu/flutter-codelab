1ad6f440cfc63f04e7fe65d5de14e5cd6b20dbec
hieuvu99 <star.hieu.99@gmail.com>
2020-09-25 10:37:36 +0700

    add scaffold for item pinned suggestions

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -28,8 +28,34 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
+  void _pushSaved() {
+      Navigator.of(context).push(
         MaterialPageRoute(builder: (BuildContext context) {
+          final tiles = _saved.map((WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Pinned Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        })
+      );
+  }
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
@@ -43,11 +69,25 @@ class _RandomWordsState extends State<RandomWords> {
         });
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color:  alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+       setState(() {
+         if(alreadySaved) {
+           _saved.remove(pair);
+         } else {
+           _saved.add(pair);
+         }
+       });
+      },
     );
   }
   @override
@@ -55,6 +95,9 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
