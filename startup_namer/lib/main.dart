06800cfe93c208a7555aed5b2016b8db846d7186
hieuvu99 <star.hieu.99@gmail.com>
2020-09-25 09:24:35 +0700

    refactor scaffhold with the title and list view

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -11,15 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
