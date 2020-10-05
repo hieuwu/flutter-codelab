fbebf58ff565ea9b3f33200c37511ae3fdc828cf
hieuvu99 <star.hieu.99@gmail.com>
2020-10-05 10:10:55 +0700

    refactor: delete line break

--- layout_demo/lib/main.dart
+++ layout_demo/lib/main.dart
@@ -32,9 +32,7 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
-
-
-
+    
     Column _buildButtonColumn(Color color, IconData icon, String label) {
       return Column(
           mainAxisSize: MainAxisSize.min,
