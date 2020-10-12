c79e4729191ef75364c0299a5b93eef4db3ec0d9
hieuvu99 <star.hieu.99@gmail.com>
2020-10-12 09:01:44 +0700

    refactor: add  default tabbar

--- instagram_profile/lib/main.dart
+++ instagram_profile/lib/main.dart
@@ -29,7 +29,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
+        length: 4,
         child: Scaffold(
             appBar: AppBar(
               title: Text("Profile"),
