7f9a16d3f1bb44bed3ae8677a243854d8867b472
hieuvu99 <star.hieu.99@gmail.com>
2020-10-15 11:26:53 +0700

    refactor:  add animation for message appearance

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -43,36 +43,36 @@ class FriendlyChatApp extends StatelessWidget {
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
+  final AnimationController animationController;
   final String text;
   String _name = 'Your Name';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0, // NEW
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child:  Text(_name[0]),)
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(_name, style: Theme.of(context).textTheme.headline4),
-              Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: Text(text),
-              )
-            ]
-          )
-        ]
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(
+                child: Text(_name[0]),
+              )),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(_name, style: Theme.of(context).textTheme.headline4),
+            Container(
+              margin: EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            )
+          ])
+        ]),
       ),
     );
   }
 }
-
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({
