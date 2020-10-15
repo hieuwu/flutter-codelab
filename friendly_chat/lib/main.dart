bffad90311be934e191ed27cd9e1ed60ffbd1f55
hieuvu99 <star.hieu.99@gmail.com>
2020-10-15 11:49:28 +0700

    refactor: add expanded widget to handle long text

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -57,13 +57,16 @@ class ChatMessage extends StatelessWidget {
               child: CircleAvatar(
                 child: Text(_name[0]),
               )),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text(_name, style: Theme.of(context).textTheme.headline4),
-            Container(
-              margin: EdgeInsets.only(top: 5.0),
-              child: Text(text),
-            )
-          ])
+          Expanded(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              )
+            ]),
+          )
         ]),
       ),
     );
