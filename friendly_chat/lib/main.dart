41185d2aaec3d17f97fa614b7ac76acd982ee76a
hieuvu99 <star.hieu.99@gmail.com>
2020-10-15 11:48:31 +0700

    refactor: import theme for ios and android platform

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // NEW
+import 'package:flutter/cupertino.dart'; // NEW
 
 void main() {
   runApp(MyApp());
 }
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
